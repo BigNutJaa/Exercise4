ea095651eff3989e2d988e011811c4dcef8c2b32
BigNutJaa <akkhaphat.i@hotmail.com>
2022-10-03 14:45:14 +0700

    newCode

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Exercise4
 
 go 1.19
+
+require github.com/BigNutJaa/sortnumASC v0.0.1
--- main.go
+++ main.go
@@ -1,48 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"sort"
-	"strconv"
+	"github.com/BigNutJaa/sortnumASC"
+	"log"
 )
 
-var number string
-
-type sortRunes []rune
-
-func (s sortRunes) Less(i, j int) bool {
-	return s[i] < s[j]
-}
-
-func (s sortRunes) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s sortRunes) Len() int {
-	return len(s)
-}
-
-func SortString(s string) string {
-	r := []rune(s)
-	sort.Sort(sortRunes(r))
-	return string(r)
-}
-
 func main() {
-	for {
-		fmt.Println("Enter some number:")
-		fmt.Scanln(&number)
-		if _, err := strconv.ParseFloat(number, 64); err != nil {
-			fmt.Printf("%q is not a number.\n", number)
-		} else {
-			break
 
-		}
+	result, err := sortnumASC.SortB("52716")
+	if err != nil {
+		log.Println("error:", err)
 	}
-
-	newNumber := []rune(number)
-	sort.Slice(newNumber, func(i int, j int) bool { return newNumber[i] < newNumber[j] })
-	fmt.Println("Input number =", number)
-	fmt.Println("New number(sort asc) =", string(newNumber))
-
+	log.Println(result)
 }
